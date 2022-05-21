bedaac90c1b81fe5e5adbd53d73ac5018f95099d
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-05-21 15:31:45 +0200

    Add current time

--- server.go
+++ server.go
@@ -2,10 +2,13 @@ package main
 
 import ("fmt"
         "net/http"
+				"time"
         )
 
 func time_request(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello world!")
+  w.Header().Set("Content-Type", "application/json");
+	currentTime := time.Now().Format(time.RFC3339);
+	fmt.Fprintf(w, currentTime);
 }
 
 func handleRequest() {
