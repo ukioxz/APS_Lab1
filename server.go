745ab44646b918aa081e2d7eef9163d2a13e6072
ukioxz <ikatember@gmail.com>
2022-05-21 16:48:27 +0300

    Add json output of time

--- server.go
+++ server.go
@@ -1,20 +1,23 @@
 package main
 
-import ("fmt"
+import (
         "net/http"
 				"time"
+        "encoding/json"
         )
 
 func time_request(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json");
 	currentTime := time.Now().Format(time.RFC3339);
-  fmt.Fprintf(w,currentTime)
+  resp := make(map[string]string);
+	resp["time"] = currentTime;
+  json.NewEncoder(w).Encode(resp);
 
 }
 
 func handleRequest() {
-  http.HandleFunc("/time", time_request)
-  http.ListenAndServe(":8795", nil)
+  http.HandleFunc("/time", time_request);
+  http.ListenAndServe(":8795", nil);
 }
 
 func main() {
