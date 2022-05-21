e216e3e718dcbc2f93875d5c6896f7f79640339f
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-05-21 12:28:32 +0200

    Create simple server

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import ("fmt"
+        "net/http"
+        )
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello world!")
+}
+
+func handleRequest() {
+  http.HandleFunc("/", home_page)
+  http.ListenAndServe(":8795", nil)
+}
+
+func main() {
+  handleRequest()
+}
