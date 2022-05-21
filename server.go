5d7329b8f4b3eef05241b0aef0c68a465c99becf
ukioxz <ikatember@gmail.com>
2022-05-21 16:36:51 +0300

    Fix string

--- server.go
+++ server.go
@@ -12,7 +12,7 @@ func time_request(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC3339);
   resp := make(map[string]string);
 	resp["time"] = currentTime;
-  fmt.Fprintf(w, "It's current time!");
+  fmt.Fprintf(w, "It's the current time!");
   json.NewEncoder(w).Encode(resp);
 }
 
