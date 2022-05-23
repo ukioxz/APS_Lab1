a2ba5086046ab337eae49800ea43fbf739c887b1
Anastasiia Sanina <saninaanstasia97@gmail.com>
2022-05-23 09:46:41 +0200

    delete empty string

--- server.go
+++ server.go
@@ -11,6 +11,7 @@ func time_request(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "application/json");
 	currentTime := time.Now().Format(time.RFC3339);
   resp := make(map[string]string);
+
 	resp["time"] = currentTime;
   fmt.Fprintf(w, "It's the current time!");
   json.NewEncoder(w).Encode(resp);
