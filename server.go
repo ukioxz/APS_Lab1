a81836ec386d7399de933ebc4100214210301e83
ukioxz <ikatember@gmail.com>
2022-05-24 10:36:30 +0300

    Formatting code with gofmt

--- server.go
+++ server.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-        "net/http"
-				"time"
-        "encoding/json"
-        "fmt"
-        )
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func time_request(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json");
-	currentTime := time.Now().Format(time.RFC3339);
-  resp := make(map[string]string);
+	w.Header().Set("Content-Type", "application/json")
+	currentTime := time.Now().Format(time.RFC3339)
+	resp := make(map[string]string)
 
-	resp["time"] = currentTime;
-  fmt.Fprintf(w, "It's the current time!");
-  json.NewEncoder(w).Encode(resp);
+	resp["time"] = currentTime
+	fmt.Fprintf(w, "It's the current time!")
+	json.NewEncoder(w).Encode(resp)
 }
 
 func handle_request() {
-  http.HandleFunc("/time", time_request);
-  http.ListenAndServe(":8795", nil);
+	http.HandleFunc("/time", time_request)
+	http.ListenAndServe(":8795", nil)
 }
 
 func main() {
-  handleRequest();
+	handleRequest()
 }
